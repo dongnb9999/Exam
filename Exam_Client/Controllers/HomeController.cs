efed62e25f3ab6cccc354f5d7d0176b5995778df
quocanh0106 <anhdqth1804005@fpt.edu.vn>
2019-12-19 00:04:52 +0700

    em cam on thay

---
+++ Exam_Client/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using StudentServiceClient = Exam_Client.ServiceReference1.StudentServiceClient;
+
+namespace Exam_EAP_Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Exam_Client.ServiceReference1.StudentServiceClient client = new StudentServiceClient();
+            var student = client.GetStudentList().ToList();
+            return View(student);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Exam_Client/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Exam_Client.MvcApplication" Language="C#" %>
